fc8fb0b0f5e5ee3b1491810b21fe391fb59acd95
Rafanii <12350120316@students.uin-suska.ac.id>
2024-10-21 13:14:37 +0700

    Add files via upload

---
+++ BrowserHistory/src/BrowserHistory.java
@@ -0,0 +1,56 @@
+
+import java.util.LinkedList;
+
+public class BrowserHistory {
+    private LinkedList<String> history;
+
+    public BrowserHistory() {
+        history = new LinkedList<>();
+    }
+
+    public void view() {
+        if (history.isEmpty()) {
+            System.out.println("History browser kosong.");
+            return;
+        }
+        System.out.println("History Browser (dari yang terbaru):");
+        for (int i = history.size() - 1; i >= 0; i--) {
+            System.out.println(history.get(i));
+        }
+    }
+
+    public void browse(String website) {
+        history.add(website);
+        System.out.println("Mengunjungi: " + website);
+    }
+
+    public void back() {
+        if (history.isEmpty()) {
+            System.out.println("Tidak ada history untuk kembali.");
+            return;
+        }
+        String lastVisited = history.removeLast();
+        System.out.println("Kembali dari: " + lastVisited);
+    }
+
+    public static void main(String[] args) {
+        BrowserHistory browserHistory = new BrowserHistory();
+
+        browserHistory.browse("www.youtube.com");
+        browserHistory.browse("www.canva.com");
+        browserHistory.browse("www.yahoo.com");
+
+        browserHistory.view();
+
+        browserHistory.back();
+        browserHistory.view();
+
+        browserHistory.back();
+        browserHistory.view();
+
+        browserHistory.back();
+        browserHistory.view();
+
+        browserHistory.back();
+    }
+}
